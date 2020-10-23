fb6ba51e6641fb33b9f1536897a8e617694f0187
MrTops <43430084+MrTops@users.noreply.github.com>
2020-10-23 11:01:17 -0500

    added error checking, should now go infinte

--- hack.js
+++ hack.js
@@ -1,8 +1,20 @@
 let getCard = () => {
-    let solution = eval(document.getElementsByClassName("card-title")[0].innerHTML.replace("x", "*").split("=")[0]);
-    let cards = document.getElementsByClassName("card-button")
-    if (cards[0].innerHTML == solution) cards[0].click();
-    if (cards[1].innerHTML == solution) cards[1].click();
-    if (cards[2].innerHTML == solution) cards[2].click();
-    if (cards[3].innerHTML == solution) cards[3].click();
+    try {
+        let solution = eval(document.getElementsByClassName("card-title")[0].innerHTML.replace("x", "*").split("=")[0]);
+        let cards = document.getElementsByClassName("card-button");
+        if (cards[0].innerHTML == solution) cards[0].click();
+        else if (cards[1].innerHTML == solution) cards[1].click();
+        else if (cards[2].innerHTML == solution) cards[2].click();
+        else if (cards[3].innerHTML == solution) cards[3].click();
+        else cards[0].click();
+    }
+    catch {
+        try {
+            document.getElementsByClassName("card-button")[0].click();
+        }
+        catch {
+            alert("FATAL ERROR");
+        }
+    }
 };
+setInterval(getCard, 1000);
